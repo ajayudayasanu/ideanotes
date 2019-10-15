6a97b78a7ea51d5c570de84db25c3be6128be15a
Ajay U <ajayudayasanu95@gmail.com>
2019-10-15 12:20:59 +0530

    fixed the password issue

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb+srv://ideas_DB_user:<password>@cluster0-xzxm7.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ideas_DB_user:ajaymongodb@123@cluster0-xzxm7.mongodb.net/test?retryWrites=true&w=majority'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
