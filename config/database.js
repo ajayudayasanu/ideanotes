794139393e4703cf13b823a8261fe258be32fc9b
Ajay U <ajayudayasanu95@gmail.com>
2019-10-15 12:09:49 +0530

    inital commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI: 'mongodb+srv://ideas_DB_user:<password>@cluster0-xzxm7.mongodb.net/test?retryWrites=true&w=majority'
+  };
+} else {
+  module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
+}
