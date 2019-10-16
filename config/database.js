998c17c54bdac85ef22a5af6bdf96f2cc893cd74
Ajay U <ajayudayasanu95@gmail.com>
2019-10-16 12:59:20 +0530

    removed extentions on the connection string

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://ideas_DB_user:ajaymongodb123@cluster0-xzxm7.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://ideas_DB_user:ajaymongodb123@cluster0-xzxm7.mongodb.net'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
